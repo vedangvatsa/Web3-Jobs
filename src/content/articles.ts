f8f6d694529aa71590030b5e35d05056b178227e
Vedang R. Vatsa <vatsavedang@gmail.com>
2025-08-11 11:59:07 +0000

    remove these -
    How to Get a Job in Web3: A Comprehensive Guide
    
    Web3 D

--- src/content/articles.ts
+++ src/content/articles.ts
@@ -1,16 +1,10 @@
 
 import type { Article } from '@/types';
-import { article as howToGetAJobInWeb3 } from './articles/how-to-get-a-job-in-web3';
 import { article as topWeb3CompaniesHiring } from './articles/top-web3-companies-hiring';
-import { article as web3DeveloperSalaryGuide } from './articles/web3-developer-salary-guide';
-import { article as blockchainDeveloperRoadmap } from './articles/blockchain-developer-roadmap';
 import { article as nonTechnicalRolesInCrypto } from './articles/non-technical-roles-in-crypto';
 import { article as guideToSmartContractAuditing } from './articles/guide-to-smart-contract-auditing';
-import { article as defiJobsGuide } from './articles/defi-jobs-guide';
-import { article as nftMarketplaceCareers } from './articles/nft-marketplace-careers';
 import { article as web3CommunityManagerJobs } from './articles/web3-community-manager-jobs';
 import { article as technicalWritingForWeb3 } from './articles/technical-writing-for-web3';
-import { article as web3MarketingGuide } from './articles/web3-marketing-guide';
 import { article as remoteWeb3Jobs } from './articles/remote-web3-jobs';
 import { article as web3InternshipsForStudents } from './articles/web3-internships-for-students';
 import { article as web3GamingJobs } from './articles/web3-gaming-jobs';
@@ -27,17 +21,11 @@ import { article as web3DesignCareers } from './articles/web3-design-careers';
 
 
 const articles: Article[] = [
-  howToGetAJobInWeb3,
   topWeb3CompaniesHiring,
-  web3DeveloperSalaryGuide,
-  blockchainDeveloperRoadmap,
   nonTechnicalRolesInCrypto,
   guideToSmartContractAuditing,
-  defiJobsGuide,
-  nftMarketplaceCareers,
   web3CommunityManagerJobs,
   technicalWritingForWeb3,
-  web3MarketingGuide,
   remoteWeb3Jobs,
   web3InternshipsForStudents,
   web3GamingJobs,
