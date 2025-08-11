915532e893ce079fed1e2607aab7969b1dae39df
Vedang R. Vatsa <vatsavedang@gmail.com>
2025-08-11 20:40:24 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="flex f

--- src/components/trusted-by.tsx
+++ src/components/trusted-by.tsx
@@ -2,6 +2,15 @@
 import Image from 'next/image';
 
 export function TrustedBy() {
+    const logos = [
+        { name: 'Coinbase', src: '/logo/coinbase.png' },
+        { name: 'Bitget', src: '/logo/bitget.png' },
+        { name: 'Binance', src: '/logo/binance.png' },
+        { name: 'LBank', src: '/logo/lbank.png' },
+        { name: 'dYdX', src: '/logo/dydx.png' },
+        { name: 'Glassnode', src: '/logo/glassnode.png' },
+    ];
+
     return (
         <section className="py-12 sm:py-16 lg:py-20">
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -12,17 +21,19 @@ export function TrustedBy() {
 
                     <div className="mt-8 flow-root">
                         <div className="-my-2 -mx-4 flex flex-wrap items-center justify-center sm:-mx-6 lg:-mx-8">
-                            <div className="py-4 px-6 sm:px-6 lg:px-8">
-                                <Image 
-                                  src="/logo/coinbase.png" 
-                                  alt="Coinbase" 
-                                  width={110} 
-                                  height={28}
-                                  className="h-7 w-auto transition-opacity opacity-60 hover:opacity-100"
-                                  unoptimized
-                                />
-                                <span className="sr-only">Coinbase</span>
-                            </div>
+                            {logos.map((logo) => (
+                                <div key={logo.name} className="py-4 px-6 sm:px-6 lg:px-8">
+                                    <Image 
+                                      src={logo.src}
+                                      alt={logo.name} 
+                                      width={130} 
+                                      height={32}
+                                      className="h-8 w-auto transition-opacity opacity-60 hover:opacity-100"
+                                      unoptimized
+                                    />
+                                    <span className="sr-only">{logo.name}</span>
+                                </div>
+                            ))}
                         </div>
                     </div>
                 </div>
