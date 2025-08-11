aa9790e2585ea6ae1e1e03a5db5f7fc1bca644df
Vedang R. Vatsa <vatsavedang@gmail.com>
2025-08-11 20:53:34 +0000

    there are still two cross icons

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -57,12 +57,6 @@ export function Header() {
                                 <Link href="/" className="text-lg font-bold tracking-tight text-foreground" aria-label="Hashtag Web3 Homepage">
                                     Hashtag Web3
                                 </Link>
-                                <SheetClose asChild>
-                                    <Button variant="ghost" className="h-6 w-6 p-0">
-                                      <X className="h-6 w-6 text-muted-foreground" />
-                                      <span className="sr-only">Close</span>
-                                    </Button>
-                                </SheetClose>
                             </div>
                             <nav className="flex-grow flex flex-col p-4">
                                 <div className="flex-grow space-y-2">
