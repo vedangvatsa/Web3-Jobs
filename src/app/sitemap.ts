8b04ce6f1e12f5980c1adc2aec6f96df53ccac1e
Vedang R. Vatsa <vatsavedang@gmail.com>
2025-08-12 15:44:40 +0000

    Make a new community page and put all that you find in https://hashtagwe

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -17,6 +17,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.8
+    },
+    {
+      url: `${siteUrl}/community`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
     }
   ];
 
