2e4f3211827ba7fb7c53da37a835271a504219c1
Vedang R. Vatsa <vatsavedang@gmail.com>
2025-08-13 22:06:25 +0000

    Solve all these problems permanently by ensuring best quality coding

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,6 +5,6 @@ import { getAllArticles } from '@/lib/articles';
 import { FooterContent } from './footer-content';
 
 export async function Footer() {
-  const latestArticles = (await getAllArticles()).slice(0, 4);
+  const latestArticles = (await getAllArticles()).slice(0, 5);
   return <FooterContent latestArticles={latestArticles} />;
 }
