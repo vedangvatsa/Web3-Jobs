92631ea1c1b929f39dcac513d997e1d9b6d3b988
Vedang R. Vatsa <vatsavedang@gmail.com>
2025-08-15 20:15:54 +0000

    this is how the preview looked. Why is Hashtag Web3 showing twice

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,8 +34,6 @@ export const metadata: Metadata = {
     type: "website",
     locale: "en_US",
     url: siteConfig.url,
-    title: siteConfig.name,
-    description: siteConfig.description,
     siteName: siteConfig.name,
     images: [
       {
@@ -48,8 +46,6 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    title: siteConfig.name,
-    description: siteConfig.description,
     creator: "@hashtag_web3",
     images: [`${siteConfig.url}/og-image.png`],
   },
