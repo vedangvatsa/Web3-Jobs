06c1dea104d51622e33686047ee41cf94e40fe52
Vedang R. Vatsa <vatsavedang@gmail.com>
2025-08-15 21:36:51 +0000

    delete the requests for apple-touch-icon.png (for Apple devices, 180x180

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -73,12 +73,7 @@ export default async function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning className={`${inter.variable}`}>
         <head>
-            <link rel="preload" href={inter.style.fontFamily} as="font" type="font/woff2" crossOrigin="anonymous" />
-            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-            <link rel="manifest" href="/site.webmanifest" />
-            <link rel="shortcut icon" href="/favicon.ico" />
+            <link rel="icon" href="/favicon.ico" sizes="any" />
         </head>
       <body 
         className={cn('min-h-screen bg-background font-body antialiased flex flex-col')}
