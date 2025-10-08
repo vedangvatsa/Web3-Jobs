302f523816114eb7e4810e20891d13e48fde9b8b
Vedang R. Vatsa <vatsavedang@gmail.com>
2025-10-08 14:20:48 +0000

    check for other pages alsp

--- src/app/digital-nomad-visas/layout.tsx
+++ src/app/digital-nomad-visas/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
     url: 'https://hashtagweb3.com/digital-nomad-visas',
     images: [
       {
-        url: 'https://hashtagweb3.com/logo/previews/Hashtag%20Web3%20Community.jpeg',
+        url: 'https://hashtagweb3.com/og-image-tools.png',
         width: 1200,
         height: 630,
         alt: 'Digital Nomad Visa List',
@@ -24,7 +24,7 @@ export const metadata: Metadata = {
     card: 'summary_large_image',
     title: 'Digital Nomad Visa List | Hashtag Web3',
     description: 'Find the perfect country to work from with our comprehensive list of digital nomad visas.',
-    images: ['https://hashtagweb3.com/logo/previews/Hashtag%20Web3%20Community.jpeg'],
+    images: ['https://hashtagweb3.com/og-image-tools.png'],
   },
 };
 
