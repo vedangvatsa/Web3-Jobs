9096fa86b143333f5910c910479b5474951a4f22
Vedang R. Vatsa <vatsavedang@gmail.com>
2025-10-11 14:39:58 +0000

    fix it -
    Digest: sha256:bfe54feaad04f4e5d0494d28dd19491125439fb7488a04

--- workspace/src/app/layout.tsx
+++ workspace/src/app/layout.tsx
@@ -2,7 +2,7 @@
 import type { Metadata } from 'next';
 import { Toaster } from '@/components/ui/toaster';
 import { cn } from '@/lib/utils';
-import './globals.css';
+import '../globals.css';
 import { Analytics } from "@vercel/analytics/react"
 import { Inter } from 'next/font/google';
 import { ContentSecurity } from '@/components/content-security';
