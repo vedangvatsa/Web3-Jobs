82d1041fdb832eeb0e8aba6e20ed1e1c6fcbb8ba
Vedang R. Vatsa <vatsavedang@gmail.com>
2025-10-12 11:21:00 +0000

    make the background in footer to be transparent instead of white

--- src/components/footer-content.tsx
+++ src/components/footer-content.tsx
@@ -16,7 +16,7 @@ export function FooterContent({ latestArticles }: { latestArticles: Omit<Article
   ];
 
   return (
-    <footer className="w-full border-t bg-background">
+    <footer className="w-full border-t bg-transparent">
       <div className="container mx-auto px-4 py-12">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           <div className="lg:col-span-1">
