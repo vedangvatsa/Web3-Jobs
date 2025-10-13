efce93be243525df7bfad3a3ee02f3c5ddcbe3a4
Vedang R. Vatsa <vatsavedang@gmail.com>
2025-10-13 23:21:03 +0000

    make an article page about this - Build. Scale. Get Noticed. Build your

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -160,6 +160,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
+    },
+    {
+      url: `${siteUrl}/super-hackathon`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.9,
     }
   ];
 
