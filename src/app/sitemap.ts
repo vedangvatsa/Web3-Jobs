cf73974431b0f383701d606918026c7ce679791d
Vedang R. Vatsa <vatsavedang@gmail.com>
2025-10-17 18:19:03 +0000

    I am not saying to write the news article but take the short snippet tha

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,4 +1,5 @@
 
+      
 import { MetadataRoute } from 'next';
 import { getAllArticles } from '@/lib/articles';
 import { interviewData } from '@/lib/interview-questions';
@@ -165,3 +166,5 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
   return [...staticRoutes, ...articleRoutes, ...interviewQuestionRoutes];
 }
+
+    
