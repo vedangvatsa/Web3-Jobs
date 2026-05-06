b8fc572796576131d2d20b5bd59b164f7aff9d5f
vedang <vatsvedang@gmail.com>
2026-05-06 15:50:36 +0800

    chore: add hashtagweb3.com UTM link to news footer

--- scripts/telegram-news.mjs
+++ scripts/telegram-news.mjs
@@ -196,7 +196,7 @@ function formatMessage(stories) {
     return `<a href="${s.link}"><b>${headline}</b></a>. ${summary}`;
   });
 
-  return lines.join('\n\n') + '\n\n_\nWeb3 News Feed: t.me/web3newsfeed';
+  return lines.join('\n\n') + '\n\n_\n<a href="https://hashtagweb3.com/news?utm_source=telegram&amp;utm_medium=social&amp;utm_campaign=news_digest">hashtagweb3.com/news</a> | t.me/web3newsfeed';
 }
 
 function escapeHtml(text) {
